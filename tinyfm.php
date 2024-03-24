b0a12f6371c8279194ae6cf5efdd8e68d3fc5ec8
Aufanoo <112615679+Aufanoo@users.noreply.github.com>
2024-03-24 12:05:03 +0700

    Update tinyfm.php

--- tinyfm.php
+++ tinyfm.php
@@ -27,14 +27,7 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
-);
-
-// Readonly users
-// e.g. array('users', 'guest', ...)
-$readonly_users = array(
-    'user'
+    'yaoka' => '$2y$10$c6R8ZBnaFt4iLotcsrB3Le1df7OuCX5DbN25Qg06UNuc.RWMhuT0G', //admin@123
 );
 
 // Global readonly, including when auth is not being used
